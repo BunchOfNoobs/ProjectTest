32471eaf10dd55ad70b8b2a715281047e1ab81cc
SxzFxz <DmRikays@gmail.com>
2017-02-28 05:18:59 +0800

    Did some of the code changes
    Starting page
    - Create some navigation buttons
    - Format the codes
    
    Level1, Level2, Level3
    - Did a button which acts like a counter
    - Did a display that will change when the button is press
    - It will pop out a message saying that you have won the moment you
    reach 0

--- app/src/main/java/com/example/bunchofnoobs/projecttest/Level1.java
+++ app/src/main/java/com/example/bunchofnoobs/projecttest/Level1.java
@@ -2,12 +2,37 @@ package com.example.bunchofnoobs.projecttest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Level1 extends AppCompatActivity {
+
+    int minteger = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level1);
+    }
+
+    public void NumberOfClicksLeft(View view) {
+
+        minteger = minteger - 1;
+        display(minteger);
+
+        if (minteger <= 0) {
+            displayMessage("You have won");
+        }
+
+    }
+
+    private void display(int number) {
+        TextView displayInteger = (TextView) findViewById(R.id.integer_number);
+        displayInteger.setText("" + number);
+    }
+
+    private void displayMessage(String word) {
+        TextView displayInteger = (TextView) findViewById(R.id.integer_number);
+        displayInteger.setText("" + word);
     }
 }
--- app/src/main/java/com/example/bunchofnoobs/projecttest/MainActivity.java
+++ app/src/main/java/com/example/bunchofnoobs/projecttest/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.bunchofnoobs.projecttest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,20 +13,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button b1 =(Button)findViewById(R.id.btnLevel1);
+        Button b2 =(Button)findViewById(R.id.btnLevel2);
+        Button b3 =(Button)findViewById(R.id.btnLevel3);
 
-    // Example of a call to a native method
-    TextView tv = (TextView) findViewById(R.id.sample_text);
-    tv.setText(stringFromJNI());
-    }
+        b1.setOnClickListener(new View.OnClickListener() {
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+            public void onClick(View v) {
+                Intent myintent2 = new Intent(MainActivity.this,Level1.class);
+                startActivity(myintent2);
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent myintent2 = new Intent(MainActivity.this,Level2.class);
+                startActivity(myintent2);
+
+            }
+        });
+
+        b3.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent myintent2 = new Intent(MainActivity.this,Level3.class);
+                startActivity(myintent2);
+
+            }
+        });
+
+
+
     }
 }
