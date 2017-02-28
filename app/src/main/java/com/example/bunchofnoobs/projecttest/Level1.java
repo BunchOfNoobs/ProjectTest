e9a4733101facb6a71218ebd7b7b1bb1144ea968
SxzFxz <DmRikays@gmail.com>
2017-02-28 17:39:55 +0800

    Did the clicker and timer on level 1 only (needs futher improvement)
    Did it at only for the level 1, once its fully improve then i will do
    for the rest of the level

--- app/src/main/java/com/example/bunchofnoobs/projecttest/Level1.java
+++ app/src/main/java/com/example/bunchofnoobs/projecttest/Level1.java
@@ -1,38 +1,80 @@
 package com.example.bunchofnoobs.projecttest;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Level1 extends AppCompatActivity {
 
-    int minteger = 5;
+    TextView tvTime, tvClicks;
+    Button btnClick,btnStart;
+
+    CountDownTimer timer;
+    int time = 15;
+
+    int clicks = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level1);
-    }
 
-    public void NumberOfClicksLeft(View view) {
+        tvTime = (TextView) findViewById(R.id.tvTime);
+        tvClicks = (TextView) findViewById(R.id.tvClicks);
+        btnClick = (Button) findViewById(R.id.btnClick);
+        btnStart = (Button) findViewById(R.id.btnStart);
 
-        minteger = minteger - 1;
-        display(minteger);
+        btnStart.setEnabled(true);
+        btnClick.setEnabled(false);
 
-        if (minteger <= 0) {
-            displayMessage("You have won");
-        }
+        timer = new CountDownTimer(15000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished){
 
-    }
+                time --;
+                tvTime.setText("Time " + time );
 
-    private void display(int number) {
-        TextView displayInteger = (TextView) findViewById(R.id.integer_number);
-        displayInteger.setText("" + number);
-    }
+            }
 
-    private void displayMessage(String word) {
-        TextView displayInteger = (TextView) findViewById(R.id.integer_number);
-        displayInteger.setText("" + word);
+            @Override
+            public void onFinish() {
+
+                btnStart.setEnabled(true);
+                btnClick.setEnabled(false);
+                
+                tvTime.setText("Well done");
+
+            }
+        };
+
+        btnClick.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                clicks++;
+                tvClicks.setText("Clicks " + clicks );
+
+            }
+
+        });
+
+        btnStart.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                timer.start();
+                btnStart.setEnabled(false);
+                btnClick.setEnabled(true);
+                clicks =0;
+                time = 15;
+                tvTime.setText("Time " + time);
+                tvClicks.setText("Clicks " + clicks );
+
+            }
+
+        });
     }
 }
